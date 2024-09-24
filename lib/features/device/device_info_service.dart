6b65148caa09699a68f6e32c0fb0e7076176c082
michaelneale <14976+michaelneale@users.noreply.github.com>
2024-09-24 07:40:52 +0000

    goose changes

--- lib/features/device/device_info_service.dart
+++ lib/features/device/device_info_service.dart
@@ -8,7 +8,17 @@ final deviceInfoServiceProvider = Provider((ref) => DeviceInfoService());
 class DeviceInfoService {
   final _deviceInfo = DeviceInfoPlugin();
 
-  Future<bool> isPhysicalDevice() async => Platform.isIOS
-      ? (await _deviceInfo.iosInfo).isPhysicalDevice
-      : Platform.isAndroid && (await _deviceInfo.androidInfo).isPhysicalDevice;
+  Future<bool> isPhysicalDevice() async {
+  if (Platform.isIOS) {
+    return (await _deviceInfo.iosInfo).isPhysicalDevice;
+  } else if (Platform.isAndroid) {
+    return (await _deviceInfo.androidInfo).isPhysicalDevice;
+  } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    return false; // Assume non-physical device for desktop platforms
+  } else if (Platform.isFuchsia) {
+    return false; // Assume non-physical device for Fuchsia platform
+  } else {
+    return false; // Default case (including web)
+  }
+}
 }
